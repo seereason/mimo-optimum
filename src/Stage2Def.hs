1c62de46b19cc20b725d823181a37f6a603b83b3
David Fox <dsf@seereason.com>
2015-08-08 15:44:29 -0700

    Remove some bogus Updateable instances

--- src/Stage2Def.hs
+++ src/Stage2Def.hs
@@ -6,15 +6,3 @@ module Stage2Def where
 import Stage1Def
 import Stage1Gen (App)
 import MIMO.Updatable (Updatable(appForm))
-
-instance Updatable App ProgramId where
-    appForm _ = error "Updatable App ProgramId"
-
-instance Updatable App ExerciseId where
-    appForm _ = error "Updatable App ExerciseId"
-
-instance Updatable App CircuitId where
-    appForm _ = error "Updatable App CircuitId"
-
-instance Updatable App ProgramViewId where
-    appForm _ = error "Updatable App ProgramViewId"
